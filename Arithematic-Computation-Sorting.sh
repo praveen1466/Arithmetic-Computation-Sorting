6cf057bdd94d09758d7b689e6da08f5b7f99a734
praveen1466 <praveensknl7@gmail.com>
2020-11-24 12:43:03 -0800

    Added: fourth arithematci operation

--- Arithematic-Computation-Sorting.sh
+++ Arithematic-Computation-Sorting.sh
@@ -14,3 +14,6 @@ echo "Value of op2 is: $op2"
 
 op3=$(( $thirdNum + ($firstNum/$secondNum) ))
 echo "Value of op3 is: $op3"
+
+op4=$(( ($firstNum%$secondNum) + $thirdNum ))
+echo "Value of op4 is: $op4"
